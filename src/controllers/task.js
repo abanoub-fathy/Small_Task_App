329976baa3ce083a0f3e4da42cb3003293431f69
Abanoub Fathy <abanoub.fathy.me@gmail.com>
2022-03-09 19:14:49 +0200

    Allow the user to update and delete tasks

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -40,12 +40,66 @@ const createTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send(`Update task ${req.params.id}`);
+const updateTask = async (req, res) => {
+  const updates = Object.keys(req.body);
+  const validUpdates = ["description", "completed"];
+
+  // check if the update opertaion is valid
+  const isValidUpdateReq = updates.every((update) =>
+    validUpdates.includes(update)
+  );
+
+  if (!isValidUpdateReq) {
+    return res.status(400).send({ error: "Invalid update request" });
+  }
+  try {
+    // check for  valid id
+    if (!mongoose.isValidObjectId(req.params.id)) {
+      return res.status(400).send({ error: "Not valid id" });
+    }
+
+    // get the task to update
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res.status(404).send({ error: "No task with this id" });
+    }
+
+    // update the task
+    updates.forEach((update) => (task[update] = req.body[update]));
+
+    // save the changes
+    await task.save();
+
+    // return the updated task
+    res.send(task);
+  } catch (e) {
+    res.status(500).send({ error: e.message });
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.send(`Delete Task ${req.params.id}`);
+const deleteTask = async (req, res) => {
+  try {
+    // check for  valid id
+    if (!mongoose.isValidObjectId(req.params.id)) {
+      return res.status(400).send({ error: "Not valid id" });
+    }
+
+    // get the task to update
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res.status(404).send({ error: "No task with this id" });
+    }
+
+    // delete the task
+    await task.remove();
+
+    // return the deleted task
+    res.send(task);
+  } catch (e) {
+    res.status(500).send({ error: e.message });
+  }
 };
 
 module.exports = {
