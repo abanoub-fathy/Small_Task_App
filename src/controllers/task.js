903972a49661bbd1b959a1910654eed9c4b56d15
Abanoub Fathy <abanoub.fathy.me@gmail.com>
2022-03-10 18:39:21 +0200

    use asyncwrapper insted og try catch block

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -1,46 +1,35 @@
 const mongoose = require("mongoose");
 const Task = require("../models/task");
+const asyncWrapper = require("../middlewares/async");
 
-const getAllTasks = async (req, res) => {
-  try {
-    const tasks = await Task.find();
-    res.send({
-      numberOfTasks: tasks.length,
-      tasks,
-    });
-  } catch (e) {
-    res.status(500).send({ error: e.message });
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find();
+  res.send({
+    numberOfTasks: tasks.length,
+    tasks,
+  });
+});
+
+const getSingleTask = asyncWrapper(async (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    return res.status(400).send({ error: "Not valid id" });
   }
-};
 
-const getSingleTask = async (req, res) => {
-  try {
-    if (!mongoose.isValidObjectId(req.params.id)) {
-      return res.status(400).send({ error: "Not valid id" });
-    }
+  const task = await Task.findById(req.params.id);
 
-    const task = await Task.findById(req.params.id);
-
-    if (!task) {
-      return res.status(404).send({ error: "No task with this id" });
-    }
-    res.send(task);
-  } catch (e) {
-    res.status(500).send({ error: e.message });
+  if (!task) {
+    return res.status(404).send({ error: "No task with this id" });
   }
-};
+  res.send(task);
+});
 
-const createTask = async (req, res) => {
-  try {
-    const task = Task(req.body);
-    await task.save();
-    res.status(201).send(task);
-  } catch (e) {
-    res.status(400).send({ error: e.message });
-  }
-};
+const createTask = asyncWrapper(async (req, res) => {
+  const task = Task(req.body);
+  await task.save();
+  res.status(201).send(task);
+});
 
-const updateTask = async (req, res) => {
+const updateTask = asyncWrapper(async (req, res) => {
   const updates = Object.keys(req.body);
   const validUpdates = ["description", "completed"];
 
@@ -52,55 +41,48 @@ const updateTask = async (req, res) => {
   if (!isValidUpdateReq) {
     return res.status(400).send({ error: "Invalid update request" });
   }
-  try {
-    // check for  valid id
-    if (!mongoose.isValidObjectId(req.params.id)) {
-      return res.status(400).send({ error: "Not valid id" });
-    }
 
-    // get the task to update
-    const task = await Task.findById(req.params.id);
-
-    if (!task) {
-      return res.status(404).send({ error: "No task with this id" });
-    }
-
-    // update the task
-    updates.forEach((update) => (task[update] = req.body[update]));
-
-    // save the changes
-    await task.save();
-
-    // return the updated task
-    res.send(task);
-  } catch (e) {
-    res.status(500).send({ error: e.message });
+  // check for  valid id
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    return res.status(400).send({ error: "Not valid id" });
   }
-};
 
-const deleteTask = async (req, res) => {
-  try {
-    // check for  valid id
-    if (!mongoose.isValidObjectId(req.params.id)) {
-      return res.status(400).send({ error: "Not valid id" });
-    }
+  // get the task to update
+  const task = await Task.findById(req.params.id);
 
-    // get the task to delete
-    const task = await Task.findById(req.params.id);
-
-    if (!task) {
-      return res.status(404).send({ error: "No task with this id" });
-    }
-
-    // delete the task
-    await task.remove();
-
-    // return the deleted task
-    res.send(task);
-  } catch (e) {
-    res.status(500).send({ error: e.message });
+  if (!task) {
+    return res.status(404).send({ error: "No task with this id" });
   }
-};
+
+  // update the task
+  updates.forEach((update) => (task[update] = req.body[update]));
+
+  // save the changes
+  await task.save();
+
+  // return the updated task
+  res.send(task);
+});
+
+const deleteTask = asyncWrapper(async (req, res) => {
+  // check for  valid id
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    return res.status(400).send({ error: "Not valid id" });
+  }
+
+  // get the task to delete
+  const task = await Task.findById(req.params.id);
+
+  if (!task) {
+    return res.status(404).send({ error: "No task with this id" });
+  }
+
+  // delete the task
+  await task.remove();
+
+  // return the deleted task
+  res.send(task);
+});
 
 module.exports = {
   getAllTasks,
